b82010d7ea8cd83c9c022b5765aec9d1fb916f15
imenhammami12 <imen.hammami@esprit.tn>
2026-02-03 20:30:57 +0100

    ajout de la parti backoffice et la relier avec le front office avec un controle dacces basse sur les roles

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route('/admin')]
+class AdminController extends AbstractController
+{
+    /**
+     * Route par défaut pour /admin - redirige vers le login ou le dashboard
+     */
+    #[Route('', name: 'admin_index')]
+    public function index(): Response
+    {
+        // Si l'utilisateur est déjà connecté avec ROLE_ADMIN
+        if ($this->isGranted('ROLE_ADMIN')) {
+            return $this->redirectToRoute('admin_dashboard');
+        }
+        
+        // Sinon, rediriger vers le login admin
+        return $this->redirectToRoute('admin_login');
+    }
+    
+    #[Route('/dashboard', name: 'admin_dashboard')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function dashboard(): Response
+    {
+        return $this->render('admin/dashboard.html.twig', [
+            'user' => $this->getUser(),
+        ]);
+    }
+}
